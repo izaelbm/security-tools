8029a8ceda2462927421af221758163eaa55229d
Izael Magalhaes <46963796+izaelbm@users.noreply.github.com>
2025-09-09 14:12:54 -0400

    Create metabase.sh

---
+++ metabase.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# rede para os containers se verem
+sudo docker network create metabase-net || true
+
+# banco
+sudo docker run -d \
+  --name metabase-db \
+  --restart=unless-stopped \
+  --network metabase-net \
+  -e POSTGRES_USER=metabase \
+  -e POSTGRES_PASSWORD=metabase_pass \
+  -e POSTGRES_DB=metabase \
+  -v /opt/metabase-postgres:/var/lib/postgresql/data \
+  postgres:14
+
+# metabase
+sudo docker run -d \
+  --name metabase \
+  --restart=unless-stopped \
+  --network metabase-net \
+  -p 3000:3000 \
+  -e MB_DB_TYPE=postgres \
+  -e MB_DB_DBNAME=metabase \
+  -e MB_DB_USER=metabase \
+  -e MB_DB_PASS=metabase_pass \
+  -e MB_DB_HOST=metabase-db \
+  -e MB_DB_PORT=5432 \
+  -e JAVA_TOOL_OPTIONS='-Duser.timezone=America/Sao_Paulo' \
+  metabase/metabase:latest
