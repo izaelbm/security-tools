71050aa599a839ff2e4ef7ca204ca35baf33a749
Izael Magalhaes <46963796+izaelbm@users.noreply.github.com>
2025-09-25 09:54:23 -0400

    Update metabase.sh

--- metabase.sh
+++ metabase.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# rede para os containers se verem
-sudo docker network create metabase-net || true
+docker network create \
+  --driver bridge \
+  --subnet 10.43.101.0/24 \
+  --gateway 10.43.101.1 \
+  metabase-net
 
-# banco
 sudo docker run -d \
   --name metabase-db \
   --restart=unless-stopped \
   --network metabase-net \
   -e POSTGRES_USER=metabase \
-  -e POSTGRES_PASSWORD="@metabase#pass" \
+  -e POSTGRES_PASSWORD="##pegarsenhanocofre##" \
   -e POSTGRES_DB=metabase \
   -v /opt/metabase/metabase-data:/var/lib/postgresql/data \
   postgres:14
@@ -23,7 +25,7 @@ sudo docker run -d \
   -e MB_DB_TYPE=postgres \
   -e MB_DB_DBNAME=metabase \
   -e MB_DB_USER=metabase \
-  -e MB_DB_PASS="@metabase#pass" \
+  -e MB_DB_PASS="##pegarsenhanocofre##" \
   -e MB_DB_HOST=metabase-db \
   -e MB_DB_PORT=5432 \
   -e MB_PLUGINS_DIR=/plugins \
